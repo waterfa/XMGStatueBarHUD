5a6270fb467c4068f72bfb9000825ab9d6dd343d
waterfa <zhongyongfa@zhongyongfadeiMac.local>
2016-07-03 12:54:17 +0800

    初始化项目
    初始化项目

---
+++ XMGStatuBarHUD/XMGStatuBarHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMGStatuBarHUD
+//
+//  Created by 钟永发 on 16/7/3.
+//  Copyright © 2016年 facker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
